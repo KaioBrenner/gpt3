d5b6c8a7439c92c8fc385dc1ca31bbb4e0a0c6e4
Kaio Brenner <kaiobrenner460@protonmail.com>
2023-02-18 12:07:48 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar></Navbar>
+        <Header></Header>
+      </div>
+      <Brand></Brand>
+      <WhatGPT3></WhatGPT3>
+      <Features></Features>
+      <Possibility></Possibility>
+      <CTA></CTA>
+      <Blog></Blog>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
